c8db754481ab88ba56d5bb51aea7bb84d25a693d
Edmilton <edmilton.neves@gmail.com>
2017-12-08 00:26:13 -0200

    String1: exercise D: remove vars 'a_mixed' and 'b_mixed'

--- basic/string1.py
+++ basic/string1.py
@@ -57,10 +57,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    a_mixed = b[:2] + a[2:]
-    b_mixed = a[:2] + b[2:]
-    str_return = a_mixed + ' ' + b_mixed
-    return str_return
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
