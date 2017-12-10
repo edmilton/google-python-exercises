4a8558ee413933ba42706a7c004e25bdd3f20aa8
Edmilton <edmilton.neves@gmail.com>
2017-12-10 13:24:50 -0200

    Exercise D: add variables 'a_mixed' and 'b_mixed'. Add 'join' to concatenate more than 2 strings.

--- basic/string1.py
+++ basic/string1.py
@@ -57,7 +57,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+    a_mixed = b[:2] + a[2:]
+    b_mixed = a[:2] + b[2:]
+    return ' '.join([a_mixed, b_mixed])
 
 
 # Provided simple test() function used in main() to print
